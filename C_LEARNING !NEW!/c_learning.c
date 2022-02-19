df548c8c1418159d4ef0b619e9bf59ac4337c268
eunsu28 / AY_28 <ayay.e2008@gmail.com>
2022-02-19 19:40:08 +0900

    to next line

--- C_LEARNING !NEW!/c_learning.c
+++ C_LEARNING !NEW!/c_learning.c
@@ -8,7 +8,7 @@ int Sum(int value1, int value2) {
 void main(void) { //여기서 모든것을 작동 시킨다
 	printf("Hello World\n");
 	int a = 1;
-	printf("%d place get gold medal", a);
+	printf("%d place get gold medal\n", a);
 	
 	int s;
 	s = Sum(5, 11);
