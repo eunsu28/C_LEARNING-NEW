6af3cb1f71d894c77a6f2a73a35989d749494cfd
eunsu28 / AY_28 <ayay.e2008@gmail.com>
2022-02-19 19:39:31 +0900

    new function named SUM

--- C_LEARNING !NEW!/c_learning.c
+++ C_LEARNING !NEW!/c_learning.c
@@ -1,9 +1,16 @@
 ﻿#include <stdio.h>
 
-
+int Sum(int value1, int value2) {
+	int result = value1 + value2;
+	return result;
+}
 
 void main(void) { //여기서 모든것을 작동 시킨다
 	printf("Hello World\n");
 	int a = 1;
 	printf("%d place get gold medal", a);
+	
+	int s;
+	s = Sum(5, 11);
+	printf("result = %d \n", s);
 }
