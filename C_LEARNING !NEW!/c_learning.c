7178c4e9bd39617d689139eae766cb3048756112
eunsu28 / AY_28 <ayay.e2008@gmail.com>
2023-01-08 13:53:12 +0900

    back to nothing

--- C_LEARNING !NEW!/c_learning.c
+++ C_LEARNING !NEW!/c_learning.c
@@ -1,57 +1,5 @@
 ﻿#include <stdio.h>
 
-int Sum(int value1, int value2) {
-	int result = value1 + value2;
-	return result;
-}
-
-int Minus(int value1, int value2) {
-	int result = value1 - value2;
-	return result;
-}
-
-int mid_aver(int one, int two, int three, int four, int five, int six, int seven) {
-	int result = one + two + three + four + five + six + seven;
-	float devide = (result / 7);
-	return devide;
-}
-
 void main(void) {
-	printf("Hello World\n");
-	puts("HELLO MY NAME IS AYDEN");
 	
-	int s;
-	s = Sum(5, 11);
-	printf("result = %d \n", s);
-
-	int m = Minus(20, 4);
-	printf("RESULT IS %d \n", m);
-
-	int fornum = 1;
-	for (fornum = 1; fornum < 6; fornum++) {
-		printf("NUMBER: %d\n", fornum);
-	}
-	
-	int tree = 1;
-	while (tree < 11) {
-		if (tree < 10) {
-			printf("%d time you've hit the tree\n", tree);
-			tree++;
-		}
-		else {
-			printf("tree is going over\n");
-			break;
-		}
-	}
-
-	float fin = mid_aver(100, 100, 99, 92, 90, 94, 97);
-	printf("YOUR MID EXAM'S AVERAGE = %f\n", fin);
-
-	int a = Sum(100, 200);
-	if (a > 299) {
-		puts("HELLO");
-	}
-	else {
-		puts("BYE");
-	}
 }
