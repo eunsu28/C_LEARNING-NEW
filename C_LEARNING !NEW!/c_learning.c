5c28b9e529bb8e7785037c51c595ac85e90b2e3b
eunsu28 / AY_28 <ayay.e2008@gmail.com>
2023-01-20 16:59:57 +0900

    print the function

--- C_LEARNING !NEW!/c_learning.c
+++ C_LEARNING !NEW!/c_learning.c
@@ -8,9 +8,11 @@ void main(void) {
 	printf("Hello, World\n");
 	int a = 2;
 	if (a == 1) {
-		printf("HELLO");
+		printf("HELLO\n");
 	}
 	else {
-		printf("bye");
+		printf("bye\n");
 	}
+	int for_function = Sum(4, 5);
+	printf("%d\n", for_function);
 }
